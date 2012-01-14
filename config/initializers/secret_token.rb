fbdc5c98aee9f05ffca5cb7610e8bd75c10e2886
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2012-01-14 11:23:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mist::Application.config.secret_token = '35b3a99864cf26d36cc3282e3169d557f5b55b79712b27db909f008c1e46a2eca0db90aa4a415b64a83d9538f98e23966205496220ff92f4b902f515f5b1ebed'
