70c47a2b0fa3084fb6bbe46abdbc280674dea178
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2012-01-16 16:25:36 -0500

    psych compatibility fixes

--- lib/mist/git_model/attributes.rb
+++ lib/mist/git_model/attributes.rb
@@ -1,11 +1,21 @@
 class Mist::GitModel::Attributes < HashWithIndifferentAccess
   def initialize(model)
     @model = model
-    super()
+    super({})
   end
   
   def []=(key, value)
-    @model.send(:"#{key}_will_change!") unless value == self[key]
+    @model.send(:"#{key}_will_change!") unless !@model || value == self[key]
     super
+  end
+  
+  # Yamlers using Psych will call this method, we just delegate it into
+  # `HashWithIndifferentAccess`. If we don't, the yaml tag will be
+  # "!ruby/object:Mist::GitModel::Attributes" so that when the record
+  # is deserialized, this class will be instantiated, resulting in an
+  # error. (This class should only be instantiated directly by
+  # `Mist::GitModel`.)
+  def to_yaml
+    HashWithIndifferentAccess.new(self).to_yaml
   end
 end
