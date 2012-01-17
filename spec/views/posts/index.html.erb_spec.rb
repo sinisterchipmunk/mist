dcf2eeac04a984eb5cee12c9bd765fbeb46a62b2
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2012-01-16 23:27:44 -0500

    DRYer views, and index shows the full first post

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -1,14 +1,27 @@
 require 'spec_helper'
 
 describe "posts/index" do
+  NUMBERS = %w(one two three four five six seven eight nine ten)
+  
   before(:each) do
-    assign(:posts, [
-      create(:post, :title => "one"),
-      create(:post, :title => "two")
-    ])
+    list = []
+    NUMBERS.each do |n|
+      list << create(:post, :title => n, :content => "#{n}-Paragraph1\n\n#{n}-Paragraph2")
+    end
+    assign(:posts, list)
   end
 
-  it "renders a list of posts" do
+  it "renders the full content of the first post" do
     render
+    rendered.should =~ /one-Paragraph1/
+    rendered.should =~ /one-Paragraph2/
+  end
+  
+  it "renders only the first paragraph of all other posts" do
+    render
+    NUMBERS[1..-1].each do |n|
+      rendered.should =~ /#{n}-Paragraph1/
+      rendered.should_not =~ /#{n}-Paragraph2/
+    end
   end
 end
