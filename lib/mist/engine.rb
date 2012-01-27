1cee33fcb42ac6dbcbdcdaed4f1534ee6fa79d2a
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2012-01-27 09:45:54 -0500

    add mist assets to precompile

--- lib/mist/engine.rb
+++ lib/mist/engine.rb
@@ -4,5 +4,9 @@ module Mist
   class Engine < Rails::Engine
     engine_name 'mist'
     isolate_namespace Mist
+    
+    initializer "mist.assets" do |app|
+      app.config.assets.precompile += ['mist_core.js', 'mist_core.css', 'mist.js', 'mist.css']
+    end
   end
 end
